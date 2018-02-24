16dc1d5baf305d0b156181a6d2f19bf9562ffc7e
metalcated <mike.gomon@gmail.com>
2018-02-24 10:18:55 -0500

    updated to not be dependent on lsb_release

--- os-detection.sh
+++ os-detection.sh
@@ -13,9 +13,10 @@
 ##              0.5 - updated package name for redhat-lsb
 ##              0.6 - updated to support OEL (Oracle EL)
 ##              0.7 - removed version output due to issues with other scripts
+##              0.8 - updated to not be dependent on lsb_release
 ##
 #############################################################################
-ver=0.7
+ver=0.8
 
 # exit if not root
 if [[ $EUID -ne 0 ]]; then
@@ -24,33 +25,37 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # install lsb_release depending on os
-if [[ -z $(which lsb_release) ]]; then
-        # install lsb_release
-        if [[ -f /usr/bin/dnf ]]; then
-                /usr/bin/dnf install redhat-lsb -y
-        elif [[ -f /usr/bin/yum ]]; then
-                /usr/bin/yum install redhat-lsb -y
-        elif [[ -f /usr/sbin/up2date ]]; then
-                /usr/sbin/up2date -i -f redhat-lsb
+if [[ -z $(which lsb_release 2>&1|grep -v "which") ]]; then
+        if [[ $(subscription-manager status|grep "Overall Status"|awk {'print $3'}) = Current ]]; then
+                # install lsb_release
+                if [[ -f /usr/bin/dnf ]]; then
+                        /usr/bin/dnf install redhat-lsb -y
+                elif [[ -f /usr/bin/yum ]]; then
+                        /usr/bin/yum install redhat-lsb -y
+                elif [[ -f /usr/sbin/up2date ]]; then
+                        /usr/sbin/up2date -i -f redhat-lsb
+                fi
+                # distro
+                export os=$(lsb_release -si)
+                # version
+                export os_ver=$(lsb_release -sr|cut -d. -f1)
+                # arch
+                if [[ -n $(lsb_release -s|grep amd64) ]]; then
+                        export arch="x86_64"
+                else
+                        export arch="i386"
+                fi
+        else
+                export os=$(cat /etc/redhat-release|cut -d' ' -f1)
+                export os_ver=$(grep -o '[0-9].[0-9]' /etc/redhat-release|cut -d. -f1)
+                export arch=$(uname -p)
+        fi
+        # define
+        if [[ -n $(echo $os|grep 'Red\|EnterpriseEnterpriseServer') ]]; then
+                export os="RHEL"
+        elif [[ -n $(echo $os|grep -i 'OEL\|Oracle') ]]; then
+                export os="OracleLinux"
         fi
 fi
-
-# distro
-export os=$(lsb_release -si)
-if [[ -n $(echo $os|grep 'Red\|EnterpriseEnterpriseServer') ]]; then
-        export os="RHEL"
-elif [[ -n $(echo $os|grep -i 'OEL\|Oracle') ]]; then
-        export os="OracleLinux"
-fi
-
-# version
-export os_ver=$(lsb_release -sr|cut -d. -f1)
-# arch
-if [[ -n $(lsb_release -s|grep amd64) ]]; then
-        export arch="x86_64"
-else
-        export arch="i386"
-fi
-
 # uncomment to see results
 #echo $os $os_ver $arch
