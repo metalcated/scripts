56b5f4871a90415a6999ced57ad569a9fc43a042
metalcated <mike.gomon@gmail.com>
2018-03-01 09:45:51 -0500

    another logic fix for RHEL 7 detection

--- os-detection.sh
+++ os-detection.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -x
 ############################################################################
 ##
 ## Title:       Detect OS Distro and Version
 ## Author:      metalcated
 ## Date:        08/20/2015
-## Version:     0.9
+## Version:     1.0
 ##
 ## Changelog:   0.1 - Initial Release
 ##              0.2 - Added arch detection
@@ -15,9 +16,10 @@
 ##              0.7 - removed version output due to issues with other scripts
 ##              0.8 - updated to not be dependent on lsb_release
 ##              0.9 - logic fix, was not working with lsb installed
+##              1.0 - another logic fix for RHEL 7 detection
 ##
 #############################################################################
-ver=0.9
+ver=1.0
 
 # exit if not root
 if [[ $EUID -ne 0 ]]; then
@@ -49,20 +51,23 @@ run_lsb_rls()
                 export arch="i386"
         fi
 }
-
+run_nolsb_rls()
+{
+        export os=$(cat /etc/redhat-release|cut -d' ' -f1)
+        export os_ver=$(grep -o '[0-9].[0-9]' /etc/redhat-release|cut -d. -f1)
+        export arch=$(uname -p)
+}
 # install lsb_release depending on os
 if [[ -z $(which lsb_release 2>&1|grep -v "which") ]]; then
         if [[ -n $(rpm -q subscription-manager|grep ^subscription-manager) && $(subscription-manager status|grep "Overall Status"|awk {'print $3'}) = Current ]]; then
                 install_lsb_rls
-        elif [[ -z $(rpm -q subscription-manager|grep ^subscription-manager) ]]; then
-                install_lsb_rls
+        else
+                run_nolsb_rls
         fi
 elif [[ $(which lsb_release 2>&1|grep -v "which") ]]; then
         run_lsb_rls
 else
-        export os=$(cat /etc/redhat-release|cut -d' ' -f1)
-        export os_ver=$(grep -o '[0-9].[0-9]' /etc/redhat-release|cut -d. -f1)
-        export arch=$(uname -p)
+        run_nolsb_rls
 fi
 # define
 if [[ -n $(echo $os|grep 'Red\|EnterpriseEnterpriseServer') ]]; then
